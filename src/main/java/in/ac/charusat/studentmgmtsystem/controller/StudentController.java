af6424cdb759ef30d18d469759904a9bb76d64bb
20IT158 <20it158@charusat.edu.in>
2022-09-12 13:19:05 +0530

    Added CORS

--- src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins="https://localhost:3000")
 public class StudentController {
 //    List<Student> students = new ArrayList<>(
 //            Arrays.asList(
